09e92355214877293cabd88d7523af6dc893e2df
marialymperaiou <maria.lymperaiou@gmail.com>
2021-06-02 15:22:48 +0300

    update init

--- models/stylegan_ADA/__init__.py
+++ models/stylegan_ADA/__init__.py
@@ -1,16 +1,17 @@
-import sys
-import os
-import shutil
-import glob
-import platform
+# Copyright 2020 Erik Härkönen. All rights reserved.
+# This file is licensed to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+# OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+
 from pathlib import Path
+import sys
 
-current_path = os.getcwd()
+#module_path = Path(__file__).parent / 'pytorch_biggan'
+#sys.path.append(str(module_path.resolve()))
 
-module_path = Path(__file__).parent / 'stylegan_ADA'
-sys.path.append(str(module_path.resolve()))
-os.chdir(module_path)
-
-from model import Generator
-
-os.chdir(current_path)
+from .model import Generator
