5d6a889dc4daab20c042e7580679f860ebe59a22
marialymperaiou <maria.lymperaiou@gmail.com>
2021-06-02 16:30:30 +0300

    update wrapper

--- models/wrappers.py
+++ models/wrappers.py
@@ -21,6 +21,7 @@ from netdissect import proggan, zdataset
 from . import biggan
 from . import stylegan
 from . import stylegan2
+from . import stylegan_ADA
 from abc import abstractmethod, ABC as AbstractBaseClass
 from functools import singledispatch
 
@@ -92,6 +93,164 @@ class BaseModel(AbstractBaseClass, torch.nn.Module):
 
     def named_modules(self, *args, **kwargs):
         return self.model.named_modules(*args, **kwargs)
+    
+class StyleGAN_ADA(BaseModel):
+    def __init__(self, device, class_name, truncation=1.0, use_w=False):
+        super(StyleGAN_ADA, self).__init__('StyleGAN_ADA', class_name or 'ffhq')
+        self.device = device
+        self.truncation = truncation
+        self.latent_avg = None
+        self.w_primary = use_w # use W as primary latent space?
+
+        # Image widths
+        configs = {
+            # Converted NVIDIA official
+            'ffhq': 1024
+        }
+        
+        assert self.outclass in configs, \
+        f'Invalid StyleGAN_ADA class {self.outclass}, should be one of [{", ".join(configs.keys())}]'
+
+        self.resolution = configs[self.outclass]
+        self.name = f'StyleGAN_ADA-{self.outclass}'
+        self.has_latent_residual = True
+        self.load_model()
+        self.set_noise_seed(0)
+
+    def latent_space_name(self):
+        return 'W' if self.w_primary else 'Z'
+
+    def use_w(self):
+        self.w_primary = True
+
+    def use_z(self):
+        self.w_primary = False
+
+    # URLs created with https://sites.google.com/site/gdocs2direct/
+    def download_checkpoint(self, outfile):
+        checkpoints = {
+            'ffhq': 'https://drive.google.com/uc?export=download&id=1FJRwzAkV-XWbxgTwxEmEACvuqF5DsBiV'
+        }
+
+        url = checkpoints[self.outclass]
+        download_ckpt(url, outfile)
+
+    def load_model(self):
+        checkpoint_root = os.environ.get('GANCONTROL_CHECKPOINT_DIR', Path(__file__).parent / 'checkpoints')
+        checkpoint = Path(checkpoint_root) / f'stylegan_ADA/stylegan_ADA_{self.outclass}_{self.resolution}.pt'
+        
+        self.model = stylegan_ADA.Generator(self.resolution, 512, 8).to(self.device)
+
+        if not checkpoint.is_file():
+            os.makedirs(checkpoint.parent, exist_ok=True)
+            self.download_checkpoint(checkpoint)
+        
+        ckpt = torch.load(checkpoint)
+        self.model.load_state_dict(ckpt['g_ema'], strict=False)
+        self.latent_avg = ckpt['latent_avg'].to(self.device)
+
+    def sample_latent(self, n_samples=1, seed=None, truncation=None):
+        if seed is None:
+            seed = np.random.randint(np.iinfo(np.int32).max) # use (reproducible) global rand state
+
+        rng = np.random.RandomState(seed)
+        z = torch.from_numpy(
+                rng.standard_normal(512 * n_samples)
+                .reshape(n_samples, 512)).float().to(self.device) #[N, 512]
+        
+        if self.w_primary:
+            z = self.model.style(z)
+
+        return z
+
+    def get_max_latents(self):
+        return self.model.n_latent
+
+    def set_output_class(self, new_class):
+        if self.outclass != new_class:
+            raise RuntimeError('StyleGAN_ADA: cannot change output class without reloading')
+    
+    def forward(self, x):
+        x = x if isinstance(x, list) else [x]
+        out, _ = self.model(x, noise=self.noise,
+            truncation=self.truncation, truncation_latent=self.latent_avg, input_is_w=self.w_primary)
+        return 0.5*(out+1)
+
+    def partial_forward(self, x, layer_name):
+        styles = x if isinstance(x, list) else [x]
+        inject_index = None
+        noise = self.noise
+
+        if not self.w_primary:
+            styles = [self.model.style(s) for s in styles]
+
+        if len(styles) == 1:
+            # One global latent
+            inject_index = self.model.n_latent
+            latent = self.model.strided_style(styles[0].unsqueeze(1).repeat(1, inject_index, 1)) # [N, 18, 512]
+        elif len(styles) == 2:
+            # Latent mixing with two latents
+            if inject_index is None:
+                inject_index = random.randint(1, self.model.n_latent - 1)
+
+            latent = styles[0].unsqueeze(1).repeat(1, inject_index, 1)
+            latent2 = styles[1].unsqueeze(1).repeat(1, self.model.n_latent - inject_index, 1)
+
+            latent = self.model.strided_style(torch.cat([latent, latent2], 1))
+        else:
+            # One latent per layer
+            assert len(styles) == self.model.n_latent, f'Expected {self.model.n_latents} latents, got {len(styles)}'
+            styles = torch.stack(styles, dim=1) # [N, 18, 512]
+            latent = self.model.strided_style(styles)
+
+        if 'style' in layer_name:
+            return
+
+        out = self.model.input(latent)
+        if 'input' == layer_name:
+            return
+
+        out = self.model.conv1(out, latent[:, 0], noise=noise[0])
+        if 'conv1' in layer_name:
+            return
+
+        skip = self.model.to_rgb1(out, latent[:, 1])
+        if 'to_rgb1' in layer_name:
+            return
+
+        i = 1
+        noise_i = 1
+
+        for conv1, conv2, to_rgb in zip(
+            self.model.convs[::2], self.model.convs[1::2], self.model.to_rgbs
+        ):
+            out = conv1(out, latent[:, i], noise=noise[noise_i])
+            if f'convs.{i-1}' in layer_name:
+                return
+
+            out = conv2(out, latent[:, i + 1], noise=noise[noise_i + 1])
+            if f'convs.{i}' in layer_name:
+                return
+            
+            skip = to_rgb(out, latent[:, i + 2], skip)
+            if f'to_rgbs.{i//2}' in layer_name:
+                return
+
+            i += 2
+            noise_i += 2
+
+        image = skip
+
+        raise RuntimeError(f'Layer {layer_name} not encountered in partial_forward')
+
+    def set_noise_seed(self, seed):
+        torch.manual_seed(seed)
+        self.noise = [torch.randn(1, 1, 2 ** 2, 2 ** 2, device=self.device)]
+
+        for i in range(3, self.model.log_size + 1):
+            for _ in range(2):
+                self.noise.append(torch.randn(1, 1, 2 ** i, 2 ** i, device=self.device))
+
 
 # PyTorch port of StyleGAN 2
 class StyleGAN2(BaseModel):
@@ -678,6 +837,8 @@ def get_model(name, output_class, device, **kwargs):
         model = StyleGAN(device, class_name=output_class)
     elif name == 'StyleGAN2':
         model = StyleGAN2(device, class_name=output_class)
+    elif name == 'StyleGAN_ADA':
+        model = StyleGAN_ADA(device, class_name=output_class)
     else:
         raise RuntimeError(f'Unknown model {name}')
 
